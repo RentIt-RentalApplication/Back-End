f1338d5b26455563dd07b60100df297062ae0ca5
Gokul1909 <gokul@terzocloud.com>
2024-12-27 07:37:41 +0530

    BE initial structure

---
+++ src/main/java/com/rentit/rentit_service/RentitServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rentit.rentit_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentitServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentitServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rentit/rentit_service/RentitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentit.rentit_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
